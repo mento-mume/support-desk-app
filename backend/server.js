26068e62d88f031a80964218ad6eb5c735faa0d9
mento-mume <akpankevman@gmail.com>
2022-10-30 12:20:14 +0100

    preparation for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const colors = require("colors");
 const dotenv = require("dotenv").config();
@@ -13,12 +14,22 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => {
-  res.status(200).json({ message: "welcome to support desk api" });
-});
 //Routes
 app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/tickets", require("./routes/ticketRoutes"));
+
+//serve frontend
+if (process.env.NODE_ENV === "production") {
+  //set build folder
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+  app.get("*", (req, res) =>
+    res.sendFile(__dir, "../", "frontend", "build", "index.html")
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.status(200).json({ message: "welcome to support desk api" });
+  });
+}
 app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server started on ${PORT}`));
